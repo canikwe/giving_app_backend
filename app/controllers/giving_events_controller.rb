ae7f1b8a17a358af33222ab16cd442c25fd6f0c4
canikwe <c.anikwe@gmail.com>
2020-05-30 16:17:05 -0400

    update route working

--- app/controllers/giving_events_controller.rb
+++ app/controllers/giving_events_controller.rb
@@ -1,4 +1,5 @@
 class GivingEventsController < ApplicationController
+  before_action :get_giving_event, only: :update
 
   def index
     giving_events = GivingEvent.all
@@ -15,7 +16,19 @@ class GivingEventsController < ApplicationController
     end
   end
 
+  def update
+    if @giving_event.update(giving_event_params)
+      render json: @giving_event.to_json(giving_event_serializer)
+    else
+      render json: {message: 'Event not saved!'}
+    end
+  end
+
   private
+
+  def get_giving_event
+    @giving_event = GivingEvent.find(params[:id])
+  end
 
   def giving_event_params
     params.require(:giving_event).permit(:name, :description, :target_amount, :start_date, :end_date, :organization_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :organizations, only: [:index, :create]
-  resources :giving_events, only: [:create, :index]
+  resources :giving_events, only: [:create, :index, :update]
   resources :donations, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
