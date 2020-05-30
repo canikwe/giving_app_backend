4b8087620a7d2e75c1b7226c4df8f06e16380de0
canikwe <c.anikwe@gmail.com>
2020-05-30 15:31:03 -0400

    create donation working

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,28 @@
 class DonationsController < ApplicationController
+
+  def index
+    donations = Donation.all
+    render json: donations.to_json(donation_serializer)
+  end
+
+  def create
+    donation = Donation.new(donation_params)
+
+    if donation.valid?
+      donation.save
+
+      render json: donation.to_json(donation_serializer)
+    else
+      render json: {message: 'Error adding donation'}
+    end
+  end
+
+  private
+  def donation_params
+    params.require(:donation).permit(:amount, :donor_id, :giving_event_id)
+  end
+
+  def donation_serializer
+    {except: :updated_at}
+  end
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,4 @@
 class Donation < ApplicationRecord
-  belongs_to :donor, class_name: 'User'
+  belongs_to :donor, class_name: 'User', optional: true
   belongs_to :giving_event
 end
