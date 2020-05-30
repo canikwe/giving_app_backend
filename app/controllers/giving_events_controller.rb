0db7abed6cc1df8adec41778babf768ece089f48
canikwe <c.anikwe@gmail.com>
2020-05-30 19:25:34 -0400

    giving events validations working

--- app/controllers/giving_events_controller.rb
+++ app/controllers/giving_events_controller.rb
@@ -3,29 +3,29 @@ class GivingEventsController < ApplicationController
 
   def index
     giving_events = GivingEvent.all
-    render json: giving_events.to_json(giving_event_serializer)
+    render json: giving_events.to_json(giving_event_serializer), status: :ok
   end
 
   def index
     giving_events = GivingEvent.all
-    render json: giving_events.to_json(giving_event_serializer)
+    render json: giving_events.to_json(giving_event_serializer), status: :ok
   end
 
   def create
     giving_event = GivingEvent.new(giving_event_params)
     if giving_event.valid?
       giving_event.save
-      render json: giving_event.to_json(giving_event_serializer)
+      render json: giving_event.to_json(giving_event_serializer), status: :created
     else
-      render json: {message: 'Event not saved!'}
+      render json: {message: giving_event.errors.full_messages}, status: :precondition_failed
     end
   end
 
   def update
     if @giving_event.update(giving_event_params)
-      render json: @giving_event.to_json(giving_event_serializer)
+      render json: @giving_event.to_json(giving_event_serializer), status: :accepted
     else
-      render json: {message: 'Event not saved!'}
+      render json: {message: @giving_event.errors.full_messages}, status: :precondition_failed
     end
   end
 
--- app/models/giving_event.rb
+++ app/models/giving_event.rb
@@ -1,4 +1,15 @@
 class GivingEvent < ApplicationRecord
   belongs_to :organization
   has_many :donations
+
+  validates :name, presence: true
+  validates :target_amount, numericality: { only_integer: true, greater_than: 5 }
+  validate :start_date_should_be_before_end_date
+
+  def start_date_should_be_before_end_date
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:start_date, "must be before the end date")
+    end 
+  end
+
 end
