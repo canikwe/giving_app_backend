711ac7409998024f8430be9cdd232f959b744e6d
canikwe <c.anikwe@gmail.com>
2020-05-30 11:14:38 -0400

    giving event create route and action is working

--- app/controllers/giving_events_controller.rb
+++ app/controllers/giving_events_controller.rb
@@ -1,2 +1,22 @@
 class GivingEventsController < ApplicationController
+
+  def create
+    giving_event = GivingEvent.new(giving_event_params)
+    if giving_event.valid?
+      giving_event.save
+      render json: giving_event.to_json(giving_event_serializer)
+    else
+      render json: {message: 'Event not saved!'}
+    end
+  end
+
+  private
+
+  def giving_event_params
+    params.require(:giving_event).permit(:name, :description, :target_amount, :start_date, :end_date, :organization_id)
+  end
+
+  def giving_event_serializer
+    {except: [:created_at, :updated_at]}
+  end
 end
