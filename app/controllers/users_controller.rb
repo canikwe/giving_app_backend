64b0caeb975902c717d07f0ded521bcfd8e462e5
canikwe <c.anikwe@gmail.com>
2020-05-30 17:27:13 -0400

    login route working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,34 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users.to_json(except: [:created_at, :updated_at])
+    render json: users.to_json(user_serializer)
   end
 
   def show
-    render json: @user.to_json(except: [:created_at, :updated_at])
+    render json: @user.to_json(user_serializer)
+  end
+
+  def login
+    user = User.find_by(user_params)
+    if !user.nil?
+      render json: user.to_json(user_serializer)
+    else
+      render json: {message: 'Please try again'}
+    end
+  
   end
 
   private
 
   def get_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def user_serializer
+    {except: [:created_at, :updated_at]}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :organizations, only: [:index, :create]
   resources :giving_events, only: [:create, :index, :update]
   resources :donations, only: [:index, :create]
+  post 'login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
