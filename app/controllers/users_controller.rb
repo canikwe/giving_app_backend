f7f7f86fdce5aa5fc6c9a58e824166ffc149229e
canikwe <c.anikwe@gmail.com>
2020-05-30 18:22:07 -0400

    user create working with validations and status codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,30 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users.to_json(user_serializer)
+    render json: users.to_json(user_serializer), status: :ok
   end
 
   def show
-    render json: @user.to_json(user_serializer)
+    render json: @user.to_json(user_serializer), status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save
+      render json: user.to_json(user_serializer), status: :created
+    else
+      render json: {message: user.errors.full_messages}, status: :not_acceptable
+    end
   end
 
   def login
     user = User.find_by(user_params)
     if !user.nil?
-      render json: user.to_json(user_serializer)
+      render json: user.to_json(user_serializer), status: :accepted
     else
-      render json: {message: 'Please try again'}
+      render json: {message: 'Error occurred logging in. Please try again.'}, status: :not_acceptable
     end
-  
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :donations, class_name: 'Donation', foreign_key: 'donor_id'
   has_many :organizations, class_name: 'Organization', foreign_key: 'admin_id'
+
+  validates :name, presence: true
 end
